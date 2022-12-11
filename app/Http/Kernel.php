1f47ed0f1224d114049eb7a9887b7bebe1b2e3a7
Mateus Neri <mateuslimaneri@gmail.com>
2022-12-11 02:33:50 -0300

    #021 - cors adaptation

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,6 +59,7 @@ class Kernel extends HttpKernel
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'cors' => \App\Http\Middleware\Cors::class, 
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
